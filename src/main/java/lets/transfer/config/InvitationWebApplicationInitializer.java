ebe358f3f6792e489a8498ce4cf4de14ef34ad02
Spoon(김성호) <shkim84@coupang.com>
2017-02-22 12:00:55 +0900

    Initial Commit
    - Gradle configuration for spring4, thymeleaf
    - Skeleton for spring4 mvc

---
+++ src/main/java/lets/transfer/config/InvitationWebApplicationInitializer.java
@@ -0,0 +1,20 @@
+package lets.transfer.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class InvitationWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { WebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+}
---
+++ src/main/java/lets/transfer/controller/HelloController.java
@@ -0,0 +1,14 @@
+package lets.transfer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	@RequestMapping("/hello")
+	public String hello(Model model) {
+
+		return "hello";
+	}
+}
