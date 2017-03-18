be3ec6ac7773c87438241a40cd2c7960f6dc33a0
shinkook.kim <ksbang39@gmail.com>
2017-03-18 09:38:24 +0900

    add callList page

--- src/main/java/lets/transfer/domain/sample/CallListService.java
+++ src/main/java/lets/transfer/domain/sample/CallListService.java
@@ -8,31 +8,15 @@ import java.util.List;
 
 @Service
 @Transactional
-public class SampleService {
+public class CallListService {
 	private final SampleRepository sampleRepository;
 
 	@Autowired
-	public SampleService(SampleRepository sampleRepository) {
+	public CallListService(SampleRepository sampleRepository) {
 		this.sampleRepository = sampleRepository;
-	}
-
-	public Sample save(Sample sample) {
-		return sampleRepository.save(sample);
 	}
 
 	public List<Sample> list() {
 		return sampleRepository.findAll();
-	}
-
-	public Sample get(long id) {
-		return sampleRepository.findOne(id);
-	}
-
-	public void remove(long id) {
-		sampleRepository.delete(id);
-	}
-
-	public void remove(Sample sample) {
-		sampleRepository.delete(sample);
 	}
 }
