dd6aca75e190df58b393dab703ceb5904fa06c6a
shinkook.kim <ksbang39@gmail.com>
2017-03-18 09:38:24 +0900

    add callList page

--- src/main/java/lets/transfer/controller/CallListController.java
+++ src/main/java/lets/transfer/controller/CallListController.java
@@ -1,14 +1,26 @@
 package lets.transfer.controller;
 
+import lets.transfer.domain.sample.CallListService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HelloController {
-	@RequestMapping("/hello")
-	public String hello(Model model) {
+@RequestMapping("/sample")
+public class CallListController {
+	private final CallListService callListService;
 
-		return "hello";
+
+	@Autowired
+	public CallListController(CallListService callListService) {
+
+		this.callListService = callListService;
+	}
+
+	@RequestMapping("/calllist")
+	public String viewIndex(Model model) {
+		model.addAttribute("samples",callListService.list());
+		return "sample/callList";
 	}
 }
