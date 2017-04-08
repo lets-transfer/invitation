c30e81c944353815731ec6ebd5c02fb6c767fc80
shinkook.kim <ksbang39@gmail.com>
2017-04-08 12:32:49 +0900

    modify date format , update

--- src/main/java/lets/transfer/controller/TemplateController.java
+++ src/main/java/lets/transfer/controller/TemplateController.java
@@ -12,10 +12,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Controller
 @RequestMapping("/template")
@@ -29,7 +33,7 @@ public class TemplateController {
 		this.templateService = templateService;
 	}
 
-	@RequestMapping("/list")
+	@RequestMapping("")
 	public String viewIndex(Model model) {
 		model.addAttribute("templates", templateService.list());
 		return "template/list";
@@ -44,8 +48,20 @@ public class TemplateController {
 	@RequestMapping(value = "", method = RequestMethod.POST)
 	public String saveTemplate(@ModelAttribute Template template, RedirectAttributes redirectAttributes) {
 		redirectAttributes.addFlashAttribute("result", "saved");
+		Date current = new Date();
+		SimpleDateFormat sdf;
+
+		try{
+			sdf = new SimpleDateFormat("yyyy/MM/dd/hh:mm:ss");
+			String date_s = sdf.format(current);
+			Date date = sdf.parse(date_s);
+			template.setDate(date);
+		}catch (ParseException e){
+			e.printStackTrace();
+		}
+
 		templateService.save(template);
-		return "redirect:/template/list";
+		return "redirect:/template";
 	}
 
 	@RequestMapping(value ="/{id}", method = RequestMethod.GET)
@@ -58,13 +74,13 @@ public class TemplateController {
 	public String deleteTemplate(@PathVariable long id, RedirectAttributes redirectAttributes) {
 		redirectAttributes.addFlashAttribute("result", "Deleted");
 		templateService.remove(id);
-		return "redirect:/template/list";
+		return "redirect:/template";
 	}
 
 	@RequestMapping(value="upload", method = RequestMethod.POST)
-	public String templateUpload(@PathVariable long id, Model model, MultipartFile file, HttpServletRequest request){
+	public String templateUpload(@PathVariable long id, Model model, MultipartHttpServletRequest request){
 
-		String fileName = file.getOriginalFilename();
+		String fileName = request.getPathInfo();
 		String path = request.getContextPath();
 		File f = new File(path+"/"+fileName);
 		templateService.upload(f,id);
