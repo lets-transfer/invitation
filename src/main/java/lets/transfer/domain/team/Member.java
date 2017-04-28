b39ae614425ed40c331f38e7c60b3df2fe550059
shinkook.kim <ksbang39@gmail.com>
2017-04-29 08:50:36 +0900

    modify jpa sample

--- src/main/java/lets/transfer/domain/team/Member.java
+++ src/main/java/lets/transfer/domain/team/Member.java
@@ -2,16 +2,23 @@ package lets.transfer.domain.team;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Collection;
 
 @Entity
 @Data
 public class Member {
 	@Id @GeneratedValue
 	private long id;
-	private long teamId;
+
+
+	@OneToMany(fetch=FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinColumn(name="team_id")
+	private Collection<Team> teams;
 	private int age;
 	private String name;
+
+	public Collection<Team> getTeam(){
+		return teams;
+	}
 }
--- src/main/java/lets/transfer/domain/team/Team.java
+++ src/main/java/lets/transfer/domain/team/Team.java
@@ -2,6 +2,7 @@ package lets.transfer.domain.team;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -9,7 +10,12 @@ import javax.persistence.Id;
 @Entity
 @Data
 public class Team {
-	@Id
+
+	@Id @GeneratedValue
+	private long id;
+
+	@Column(name = "team_id")
 	private long teamId;
+
 	private String teamName;
 }
